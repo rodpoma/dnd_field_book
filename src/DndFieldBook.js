cea183e76fd15f5042c9570cde44dc05c4ff203a
Rodrigo Poma <rodpoma@gmail.com>
2020-10-01 15:53:31 -0600

    dice generator complete

---
+++ src/DndFieldBook.js
@@ -0,0 +1,12 @@
+import React from "react";
+import DiceRoll from "./components/DiceRoll.jsx";
+
+const DndFieldBook = () => {
+  return (
+    <div>
+      <DiceRoll />
+    </div>
+  );
+};
+
+export default DndFieldBook;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import DndFieldBook from "./DndFieldBook";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <DndFieldBook />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
